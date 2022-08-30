c58defe40d4d0000b8bb759f82f5fbf61eeea309
HayaM <moushaya@kkg-do.eu>
2022-08-30 10:44:50 +0200

    ._.

--- src/PropertyProject.java
+++ src/PropertyProject.java
@@ -1,3 +1,11 @@
 public class PropertyProject {
+    private String address;
+    private double width;
+    private double length;
 
+    public PropertyProject(){
+        address = "Erzbergerstraße 1";
+        width = 10;
+        length = 20.5;
+    }
 }
